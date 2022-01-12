5a54833c8014d7f7214aa35e178925fd70d97897
thinh ho <thinh.ho@gmail.com>
2022-01-12 13:31:19 -0500

    Ansible Role: gitdev

--- configure.sh
+++ configure.sh
@@ -6,11 +6,13 @@ __directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #
 # Help message
 help() {
-  echo "$0 --configuration <cfg> [--hosts <hosts.yml>] [-v|--check]"
+  echo "$0 --configuration <cfg> [--hosts <hosts.yml>] [-v|--check] [--user "full name"] [--email <email>]"
   echo "    -h | --help  Prints this help message"
   echo "    --hosts      Full path to host file"
   echo "    -v           Verbose"
   echo "    --check      Perform in check mode"
+  echo "    --user       User full name enclosed by double quotes (default ${USER})"
+  echo "    --email      User email addressed (default ${USER}@email.com)"
   echo "    Configuration Options"
   echo "        common   Base configurations"
 }
@@ -27,6 +29,8 @@ fi
 
 # default host is localhost
 __hosts=${__directory}/hosts/localhost.yml
+__username=${USER}
+__useremail="${USER}@email.com"
 
 while [ "$1" != "" ]; do
   case "$1" in
@@ -48,6 +52,16 @@ while [ "$1" != "" ]; do
     __playbook="${__directory}/playbooks/${1}.yml"
     ;;
 
+  --user)
+    shift
+    __username="${1}"
+    ;;
+
+  --email)
+    shift
+    __useremail="${1}"
+    ;;
+
   -v|--check)
     __args="${1} ${__args}"
     ;;
@@ -64,9 +78,12 @@ done
 echo "All configurations will be applied using "
 echo "playbook: ${__playbook}, hosts: ${__hosts}"
 echo "Additional args: ${__args}"
+echo "Username: ${__username}"
+echo "Email: ${__useremail}"
 read -p "Hit ENTER to continue or CTRL-C to exit"
 
 source ${__directory}/ansible.env
 ansible-playbook -i ${__hosts} \
   --user ${USER} \
+  --extra-vars "user_name=${__username} user_email=${__useremail}" \
   -k -K ${__playbook} ${__args}
