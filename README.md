13e4f5d2f4b0edb90282831d55b35a1a9b7d1217
thinh ho <thinh.ho@gmail.com>
2022-01-12 10:54:15 -0500

    Initial commit
    - init.sh
    - Ansible: common-packages

---
+++ README.md
@@ -0,0 +1,7 @@
+# ucdev
+
+> Manage all my personal configurations between development laptops and workstations.
+
+# Overview
+
+The initial tools necessary will be provided by the [init.sh](init.sh) script for various Linux distributions. Once the initial tools are installed, Ansible will be used to install and configure the development machine.
---
+++ init.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Initialization script to do a manual install of the minimum development tools.
+#
+
+set -e
+
+#
+# Help message
+help() {
+  echo "$0 <options>"
+  echo "    -h | --help    Prints this help message"
+  echo "    --ansible      Installs Ansible"
+  echo "    --vagrant      Installs Vagrant"
+}
+
+#
+# Find the Linux distribution
+init() {
+  egrep '^NAME' /etc/os-release | cut -d '=' -f2
+}
+
+#
+# Installs Ansible on Ubuntu system
+ubuntu_ansible() {
+  apt install software-properties-common
+  add-apt-repository --yes --update ppa:ansible/ansible
+  apt install -y ansible
+}
+
+#
+# Installs Vagrant on Ubuntu system
+ubuntu_vagrant() {
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+  apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  apt-get install vagrant
+}
+
+#
+# Main
+#
+
+if [ $# -eq 0 -o "$1" == "-h" -o "$1" == "--help" -o "$1" == "help" ]; then
+  help
+  exit 0
+fi
+
+if [ $UID -ne 0  ]; then
+  echo "Please execute this script as root"
+  exit 1
+fi
+
+while [ "$1" != "" ]; do
+  case "$1" in
+  --ansible|--vagrant)
+    __linux=$(init | tr -d '"')
+    __software=`echo $1 | cut -d '-' -f3`" ${__software}"
+    ;;
+
+  *)
+    echo "Error: Invalid argument $1"
+    help
+    exit 2
+    ;;
+  esac
+  shift
+done
+
+for item in ${__software}; do
+  case "$item" in
+  ansible)
+    if [ "${__linux}" == "Ubuntu" ]; then
+      ubuntu_ansible
+    fi
+    ;;
+
+  vagrant)
+    if [ "${__linux}" == "Ubuntu" ]; then
+      ubuntu_vagrant
+    fi
+    ;;
+  esac
+done
